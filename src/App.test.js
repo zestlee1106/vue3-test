ae50a873a80042e355f6d4cb7b6ae2cd57b77408
이영우 <ywlee@hanatour.com>
2022-12-05 20:18:21 +0900

    테스트 코드 추가

--- src/App.test.js
+++ src/App.test.js
@@ -8,5 +8,51 @@ describe("App", () => {
 
     expect(wrapper.find("h1").text()).toMatch("Todo App");
   });
+
+  it("renders label, input", () => {
+    const wrapper = shallowMount(App);
+
+    // '할 일 작성'이라는 텍스트가 화면에 출력됩니다.
+    expect(wrapper.find("label").text()).toMatch("할 일 작성");
+
+    // 할 일을 작성할 수 있는 'control'창 이 화면에 출력됩니다.
+    expect(wrapper.find("input").attributes("placeholder")).toMatch(
+      "할 일을 작성해주세요"
+    );
+  });
+
+  it("renders button", () => {
+    const wrapper = shallowMount(App);
+
+    // '추가하기'라는 버튼이 화면에 출력됩니다.
+    expect(wrapper.find("button").text()).toMatch("추가하기");
+  });
+
+  it("connects label and input", () => {
+    const wrapper = shallowMount(App);
+    const TODO_CONTROL = "todo-control";
+
+    expect(wrapper.find("label").attributes("for")).toMatch(TODO_CONTROL);
+    expect(wrapper.find("input").attributes("id")).toMatch(TODO_CONTROL);
+  });
+
+  it("changes input value when listens input event", async () => {
+    const wrapper = shallowMount(App);
+
+    // setValue는 아래 두 코드의 축약 api 입니다.
+    await wrapper.find("input").setValue("아무것도 안하기");
+    // wrapper.find("input").element.value = "아무것도 안하기";
+    // wrapper.find("input").trigger("input");
+
+    expect(wrapper.vm.text).toMatch("아무것도 안하기");
+  });
+
+  it("adds todo when listens '추가하기' click event", async () => {
+    const wrapper = shallowMount(App);
+
+    wrapper.find("input").setValue("아무것도 안하기");
+    await wrapper.find("button").trigger("click");
+
+    expect(wrapper.find("li").text()).toContain("아무것도 안하기");
+  });
 });
- 
